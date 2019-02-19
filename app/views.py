1c0ea40ab3fc40053ad201f65ca0bf8271311ed7
Milind Parvatia <milind1997@gmail.com>
2019-02-19 12:39:29 +0530

    error resolve

--- app/views.py
+++ app/views.py
@@ -13,7 +13,6 @@ from rest_framework import viewsets, status, generics, filters
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from django.contrib import messages
